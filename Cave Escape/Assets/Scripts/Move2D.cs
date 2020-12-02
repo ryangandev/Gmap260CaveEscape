ea0f02c1a0e7e1ef47a8193ba088cb501ddc7ea6
Jeontoo <minewahn@gmail.com>
2020-12-01 20:16:30 -0500

    MovingPlatform Fix
    Player now moves with the moving platform.

--- Cave Escape/Assets/Scripts/Move2D.cs
+++ Cave Escape/Assets/Scripts/Move2D.cs
@@ -96,5 +96,21 @@ public class Move2D : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag == "MovingPlatform")
+        {
+            transform.parent = other.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.transform.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+        }
+    }
+
 
 }
